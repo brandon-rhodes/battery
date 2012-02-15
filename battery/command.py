2d79b935fbd3643aa0963f5b7c646556c0a34c65
Brandon Rhodes <brandon@rhodesmill.org>
2012-02-15 11:07:16 -0500

    Allowed the Python version to build as a command-line option; and updated several of the default version numbers to reflect recent releases.

--- battery/command.py
+++ battery/command.py
@@ -1,10 +1,14 @@
 """Command-line battery."""
 
 import battery.build
+import sys
 
 def main():
-    for version in ('1.5.2', '1.6.1', '2.0.1', '2.1.3', '2.2.3', '2.3.7',
-                    '2.4.6', '2.5.5', '2.6.6', '2.7.1', '3.1.3', '3.2'):
+    versions = sys.argv[1:] or (
+        '1.5.2', '1.6.1', '2.0.1', '2.1.3', '2.2.3', '2.3.7',
+        '2.4.6', '2.5.6', '2.6.7', '2.7.2', '3.1.4', '3.2.2'
+        )
+    for version in versions:
         battery.build.download(version)
         battery.build.untar(version)
         battery.build.cmmi(version)
